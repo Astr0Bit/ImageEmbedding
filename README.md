948926957c9b36d78a3068142024003a2ab7ba45
Astr0Bit <the.gillarski@gmail.com>
2025-01-22 12:06:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+## Image Embedding Class
+
+Class for performing embedding on images using Google's MediaPipe library.
+This class' purpose is to calculate similarity between 2 images or video frames,
+using Google's MediaPipe library. It works by embedding the images so they
+are represented as a "grouped" array (similar to clustering), and then comparing
+these 2 arrays will return a float value (-1 to 1) representing how similar these images are.
+
+### Prerequisites:
+
+* Download this repo and create a new virtual environment (venv) using `python -m venv .venv`
+* Activate the venv and install the requirements using `pip install -r requirements.txt`
+
+## Usage:
+
+* To use this Python module, create a new .py-file and import it using `from image_embedding import ImageEmbedding`
+* Create an instance, for example `ie = ImageEmbedding()`
+
+#### Predict on images:
+
+* Create a list containing 2 paths to different images, such as `IMAGE_FILENAMES = ["<path1>", "<path2>"]`
+* Run the `.predict_images()` on your class instance, and add `IMAGE_FILENAMES` as a variable, eg.
+  `ie.predict_images(IMAGE_FILENAMES)` and you should now see a window displaying both images
+  and a similarity number. Example image below:
+
+  ![1737538745376](image/README/1737538745376.png)
+
+#### Predict on videos:
+
+* Create a list containing 2 paths to different videos, such as `VIDEO_FILENAMES = ["<path1>", "<path2>"]`
+* Run the `.predict_video()` on your class instance, and add `VIDEO_FILENAMES` as argument. You can also
+  the optional `save = True` to save the final video. Example syntax: `ie.predict_video(VIDEO_FILENAMES, save=True)`
+* See the `/examples` directory for finished example videos.
+* Example images below:
+
+  ![1737542622781](image/README/1737542622781.png)
+
+  ![1737543711729](image/README/1737543711729.png)
+
+  ![1737543743120](image/README/1737543743120.png)
---
+++ image_embedding.py
@@ -0,0 +1,232 @@
+
+import os
+
+# Image processing and plotting libraries
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Google MediaPipe imports
+import mediapipe as mp
+from mediapipe.tasks.python import BaseOptions
+from mediapipe.tasks.python.vision import ImageEmbedder, ImageEmbedderOptions
+
+class ImageEmbedding:
+
+    """
+    Class for performing image embedding using Google's MediaPipe library.
+    """
+
+    ROOT = os.path.dirname(os.path.abspath(__file__))
+    TARGET_WIDTH = 700
+    TARGET_HEIGHT = 600
+
+    def __init__(self, l2_normalize: bool = True, 
+                 quantize: bool = True) -> None:
+        """
+        Args: 
+            l2_normalize: Whether to L2 normalize the embeddings.
+            quantize: Whether to quantize the embeddings.
+
+        Returns:
+            None
+        """
+        self.model_path = os.path.join(
+            ImageEmbedding.ROOT, "mobilenet_v3_large_075_224_embedder.tflite")
+        self.base_options = BaseOptions(model_asset_path=self.model_path)
+        self.options = ImageEmbedderOptions(base_options=self.base_options,
+                                            l2_normalize=l2_normalize,
+                                            quantize=quantize)
+
+    def predict_images(self, IMAGE_FILENAMES: list | tuple[str, str]) -> None:
+        """
+        Predicts the embeddings of the input images.
+
+        Args:
+            image_paths: A list of paths to the input images.
+
+        Returns:
+            None
+        """
+        
+        TARGET_WIDTH = ImageEmbedding.TARGET_WIDTH
+        TARGET_HEIGHT = ImageEmbedding.TARGET_HEIGHT
+
+        # Create the image embedder
+        with ImageEmbedder.create_from_options(self.options) as embedder:
+    
+            # format images
+            first_image_mat = cv2.imread(IMAGE_FILENAMES[0])
+            second_image_mat = cv2.imread(IMAGE_FILENAMES[1])
+            first_image = mp.Image(image_format=mp.ImageFormat.SRGB, 
+                                   data=first_image_mat)
+            second_image = mp.Image(image_format=mp.ImageFormat.SRGB, 
+                                    data=second_image_mat)
+
+            # Resize the images
+            first_resized = cv2.resize(first_image_mat, 
+                                      (TARGET_WIDTH, TARGET_HEIGHT))
+            second_resized = cv2.resize(second_image_mat, 
+                                      (TARGET_WIDTH, TARGET_HEIGHT))
+
+            first_embedding_result = embedder.embed(first_image)
+            second_embedding_result = embedder.embed(second_image)
+
+            # calculate similarity between images
+            similarity = ImageEmbedder.cosine_similarity(
+                first_embedding_result.embeddings[0],
+                second_embedding_result.embeddings[0])
+            
+            # Create a side by side image
+            side_by_side = cv2.hconcat([first_resized, second_resized])
+
+            # Add similarity text to the frame
+            frame_center = (TARGET_WIDTH // 2 + 200, TARGET_HEIGHT - 50)
+
+            # Display the concatenated frames with text using matplotlib
+            plt.imshow(cv2.cvtColor(side_by_side, cv2.COLOR_BGR2RGB))
+            plt.text(frame_center[0], frame_center[1], 
+                     f"Similarity: {similarity:.2f}", 
+                     fontsize=12, color="white")
+            
+            plt.show()
+    
+    def predict_video(self, VIDEO_FILENAMES: list | tuple[str, str], 
+                      save: bool = False, 
+                      output_file_name: str = "similarity_side_by_side") -> None:
+        """
+        Method for taking in a list or tuple of 2 video file paths,
+        and calculating the similarity between the frames of the two videos.
+
+        Args:
+            VIDEO_FILENAMES: A list or tuple of 2 video file paths.
+            save: A boolean indicating whether to save the output video.
+        
+        Returns:
+            None
+        """
+
+        # Check that VIDEO_FILENAME is a list or tuple
+        if not isinstance(VIDEO_FILENAMES, (list, tuple)):
+            raise TypeError("VIDEO_FILENAMES must be a list or tuple.")
+
+        # Check that exactly 2 video file paths are provided
+        if len(VIDEO_FILENAMES) != 2:
+            raise ValueError("Please provide exactly 2 video file paths.")
+        
+        # Check that both paths exist and are videos
+        for video in VIDEO_FILENAMES:
+            if not os.path.exists(video):
+                raise FileNotFoundError(f"File {video} not found.")
+            if not video.endswith((".mp4", ".avi")):
+                raise ValueError(f"File {video} is not a video file.")
+            
+        # Prepare the plot for displaying the similarity for each frame
+        fig = plt.figure()
+        frame_number = 0
+        frame_numbers_list = [0]
+        similarities_list = [0]
+
+        # Get number of frames for each video
+        VIDEO_FRAMES = []
+        side_by_side_frames = []
+
+        for video in VIDEO_FILENAMES:
+            cap = cv2.VideoCapture(video)
+
+            FRAMES = []
+            for i in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
+                _, frame = cap.read()
+                FRAMES.append(frame)
+
+            print(f"Frames found in {video}: {len(FRAMES)}")
+            VIDEO_FRAMES.append(FRAMES)
+            cap.release()
+
+        # Handle missmatch in frames -> adjust number of frames
+        MIN_FRAMES = min([len(frames) for frames in VIDEO_FRAMES])
+        print("Adjusting frames for both videos. "\
+              f"New number of frames: {MIN_FRAMES}")
+        VIDEO_FRAMES = [frames[:MIN_FRAMES] for frames in VIDEO_FRAMES]
+
+        TARGET_WIDTH = ImageEmbedding.TARGET_WIDTH
+        TARGET_HEIGHT = ImageEmbedding.TARGET_HEIGHT
+
+        with ImageEmbedder.create_from_options(self.options) as embedder:
+            for i in range(MIN_FRAMES):
+                vid1_frame_mat = VIDEO_FRAMES[0][i]
+                vid2_frame_mat = VIDEO_FRAMES[1][i]
+
+                # Resize the frames to the target dimensions
+                vid1_frame_resized = cv2.resize(vid1_frame_mat, 
+                                               (TARGET_WIDTH, TARGET_HEIGHT))
+                vid2_frame_resized = cv2.resize(vid2_frame_mat, 
+                                               (TARGET_WIDTH, TARGET_HEIGHT))
+                
+                # get embeddings
+                vid1_frame = mp.Image(image_format=mp.ImageFormat.SRGB, 
+                                      data=vid1_frame_mat)
+                vid2_frame = mp.Image(image_format=mp.ImageFormat.SRGB, 
+                                      data=vid2_frame_mat)
+                first_embedding_result = embedder.embed(vid1_frame)
+                second_embedding_result = embedder.embed(vid2_frame)
+
+                # calculate similarity between frames
+                similarity = ImageEmbedder.cosine_similarity(
+                    first_embedding_result.embeddings[0],
+                    second_embedding_result.embeddings[0])
+                
+                # Append similarity and current frame number to lists
+                similarities_list.append(similarity)
+                frame_numbers_list.append(i)
+
+                # Plotting similarity data
+                # print(frame_numbers_list, similarities_list)
+                plt.plot(frame_numbers_list, similarities_list, color="blue")
+                plt.title("Similarity between frames")
+                plt.ylabel("Similarity")
+                plt.xlabel("Frame number")
+                fig.canvas.draw()
+
+                # Convert matplotlib figure to OpenCV image
+                plot = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+                plot = plot.reshape(fig.canvas.get_width_height()[::-1] + (4,)) # RGBA format
+                plot = cv2.cvtColor(plot, cv2.COLOR_RGBA2BGR) # Convert to BGR for OpenCV
+
+                # Resize the plot to the target dimensions
+                plot_resized = cv2.resize(plot, (TARGET_WIDTH * 2, TARGET_HEIGHT))
+                                
+                # TODO: Display the frames side by side using OpenCV
+                side_by_side = cv2.hconcat([vid1_frame_resized, 
+                                            vid2_frame_resized])
+                
+                side_by_side_plot = cv2.vconcat([side_by_side, plot_resized])
+
+                # Add similarity text to the frame
+                frame_center = (TARGET_WIDTH // 2 + 200, TARGET_HEIGHT - 50)
+                cv2.putText(
+                    side_by_side_plot,
+                    f"Similarity: {similarity:.2f}",
+                    frame_center,
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1.5, (255, 255, 255), 2)
+
+                if save:
+                    side_by_side_frames.append(side_by_side_plot)
+
+                # Display the concatenated frames
+                cv2.imshow("Frames", side_by_side_plot)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        if save:
+            writer = cv2.VideoWriter(
+                f"{output_file_name}.mp4",
+                cv2.VideoWriter_fourcc(*"VIDX"),
+                30, (TARGET_WIDTH*2, TARGET_HEIGHT*2))
+            
+            for frame in side_by_side_frames:
+                writer.write(frame)
+            writer.release()
+        cv2.destroyAllWindows()
